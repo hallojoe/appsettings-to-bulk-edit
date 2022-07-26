d27a555301ee7c0c88476eb7284b6c8998176ff9
Casper Korsgaard <cko@charlietango.dk>
2022-07-26 15:41:03 +0200

    Add files.
    Add converter and and app demo. Also modify readme.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# appsettings-to-bulk-edit
+# AppSettings to Azure App Service Configuration
+
 Convert `appSettings.json` to the format expected by the advanced edit feature, found in Azure App Services → Configuration.
---
+++ index.js
@@ -0,0 +1,45 @@
+const AzureConfigurationEmpty = ''
+const AzureConfigurationSeparator = "__"
+const AzureConfigurationSchemaPropertyName = "$schema"
+
+function toKeyValuePairs(appSettings, previousKey) {
+
+  if(!previousKey) previousKey = AzureConfigurationEmpty
+
+  const flatAppSettings = Object.keys(appSettings).reduce((accumulator, currentKey) => {    
+
+    const key = previousKey + currentKey  
+
+    if(key === AzureConfigurationSchemaPropertyName) return accumulator
+    
+    const currentValue = appSettings[currentKey]
+
+    if (Array.isArray(currentValue) || Object(currentValue) === currentValue) 
+      Object.assign(accumulator, toKeyValuePairs(currentValue, key + AzureConfigurationSeparator))
+    else accumulator[key] = currentValue
+
+    return accumulator
+
+  }, {})
+
+  return flatAppSettings
+
+}
+
+function toAzureConfigurationValues(appSettings) {
+
+  const flatAppSettings = toKeyValuePairs(appSettings)
+  
+  const mappedFlatAppSettings = Object.keys(flatAppSettings).map(flatAppSettingKey => {
+
+    return {
+      key: flatAppSettingKey, 
+      value: flatAppSettings[flatAppSettingKey], 
+      slotSetting: false 
+    }
+
+  })
+
+  return mappedFlatAppSettings
+
+}
